662c7b84c17818db80ad8e42a9ce8eb4f1b24656
jabezinu <jabezzinu31@gmail.com>
2025-04-24 11:05:39 +0300

    doing css of navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,24 +4,28 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-        <img src={assets.logo} alt="" />
-        <ul>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+        <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+        <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink>
-                <li>HOME</li>
-                <hr />
+                <hr className='border-none outline-none h-0.5 bg-primary w-1/5 m-auto'/>
+                <li className='py-1'>HOME</li>
+                <hr className='border-none outline-none h-0.5 w-3/5 bg-primary m-auto'/>
             </NavLink>
             <NavLink>
-                <li>ALL DOCTORS</li>
-                <hr />
+                <hr className='border-none outline-none h-0.5 bg-primary w-1/5 m-auto'/>
+                <li className='py-1'>ALL DOCTORS</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
             </NavLink>
             <NavLink>
-                <li>ABOUT</li>
-                <hr />
+                <hr className='border-none outline-none h-0.5 bg-primary w-1/5 m-auto'/>
+                <li className='py-1'>ABOUT</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
             </NavLink>
             <NavLink>
-                <li>CONTACT</li>
-                <hr />
+                <hr className='border-none outline-none h-0.5 w-1/5 m-auto bg-primary'/>
+                <li className='py-1'>CONTACT</li>
+                <hr className='border-none outline-none h-0.5 w-3/5 m-auto bg-primary'/>
             </NavLink>
         </ul>
         <div>
