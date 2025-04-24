1dcd7c90afb7fab06cce5d35d22a6e27c24c6f42
jabezinu <jabezzinu31@gmail.com>
2025-04-24 11:44:47 +0300

    still working on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const navigate = useNavigate()
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
-            <NavLink>
-                <hr className='border-none outline-none h-0.5 bg-primary w-1/5 m-auto'/>
+            <NavLink to="/">
+                {/* <hr className='border-none outline-none h-0.5 bg-primary w-1/5 m-auto hidden'/> */}
                 <li className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 w-3/5 bg-primary m-auto'/>
+                <hr className='border-none outline-none h-0.5 w-3/5 bg-primary m-auto hidden'/>
             </NavLink>
-            <NavLink>
-                <hr className='border-none outline-none h-0.5 bg-primary w-1/5 m-auto'/>
+            <NavLink to="/doctors">
+                {/* <hr className='border-none outline-none h-0.5 bg-primary w-1/5 m-auto hidden'/> */}
                 <li className='py-1'>ALL DOCTORS</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
-            <NavLink>
-                <hr className='border-none outline-none h-0.5 bg-primary w-1/5 m-auto'/>
+            <NavLink to="/about">
+                {/* <hr className='border-none outline-none h-0.5 bg-primary w-1/5 m-auto hidden'/> */}
                 <li className='py-1'>ABOUT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
-            <NavLink>
-                <hr className='border-none outline-none h-0.5 w-1/5 m-auto bg-primary'/>
+            <NavLink to="/contact">
+                {/* <hr className='border-none outline-none h-0.5 w-1/5 m-auto hidden bg-primary'/> */}
                 <li className='py-1'>CONTACT</li>
-                <hr className='border-none outline-none h-0.5 w-3/5 m-auto bg-primary'/>
+                <hr className='border-none outline-none h-0.5 w-3/5 m-auto hidden bg-primary'/>
             </NavLink>
         </ul>
-        <div>
-            <button>Create Account</button>
+        <div className='flex items-center gap-4'>
+            <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full hidden md:block font-light'>Create Account</button>
         </div>
     </div>
   )
