a7893966cb334984507d466f18f4930a66dd0957
jabezinu <jabezzinu31@gmail.com>
2025-04-25 04:34:20 +0300

    fix text

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,11 +6,10 @@ const Header = () => {
     <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg  px-6 md:px-10 lg:px-20'>
       {/* ----------Left side -------------- */}
       <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
-        {/*       */}
-        <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
+        <p className='text-3xl md:text-3xl lg:text-4xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
           Book Appointment <br /> With Trusted Doctors
         </p>
-        <div>
+        <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
           <img src={assets.group_profiles} alt="" />
           <p>Simply browse through our extesive list of trusted doctors, <br /> schedul your appointment hassle-free.</p>
         </div>
