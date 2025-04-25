87fd4daf876bdd4fc3fba86ef975e8707293ca42
jabezinu <jabezzinu31@gmail.com>
2025-04-25 05:34:58 +0300

    still working on it

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -10,7 +10,7 @@ const SpecialityMenu = () => {
     <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='speciality'>
         <h1 className='text-3xl font-medium'> Find by speciality </h1>
         <p className='sm:w-1/3 text-center text-sm'>Simpley browse through our extesive list of trusted doctors, schedule your appointment hassle-free</p>
-        <div className='flex sm:justify-center gap-4 pt-5 '>
+        <div className='flex sm:justify-center gap-4 pt-5 w-full '>
             {specialityData.map((item, index) => (
                 <Link key={index} to={`/doctors/${item.speciality}`}>
                     <img src={item.image} alt="" />
