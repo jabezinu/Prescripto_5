df2b490a33ca7f21bbe6701ebdc73637c3b3740c
jabezinu <jabezzinu31@gmail.com>
2025-04-25 05:25:38 +0300

    still working on it

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {assets, specialityData} from '../assets/assets'
+import { Link } from 'react-router-dom';
 
 const SpecialityMenu = () => {
+
+    const [speciality, setSpeciality] = useState('')
+
   return (
-    <div>SpecialityMenu</div>
+    <div id='speciality'>
+        <h1> Find by speciality </h1>
+        <p>Simpley browse through our extesive list of trusted doctors, schedule your appointment hassle-free</p>
+        <div>
+            {specialityData.map((item, index) => (
+                <Link key={index} to={`/doctors/${item.speciality}`}>
+                    <img src={item.image} alt="" />
+                    <p>{item.speciality}</p>
+                </Link>
+            ))}
+        </div>
+    </div>
   )
 }
 
